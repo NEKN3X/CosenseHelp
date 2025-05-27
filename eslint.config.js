4a405c6a12b1b7a917c5611f98d680753b0cef66
nekn3x <43030839+NEKN3X@users.noreply.github.com>
2025-05-27 20:00:33 +0900

    setup

--- eslint.config.js
+++ eslint.config.js
@@ -18,4 +18,9 @@ export default defineConfig([
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ]);
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
@@ -5,5 +6,12 @@ export default defineConfig({
   srcDir: "src",
   modules: ["@wxt-dev/module-react"],
   imports: false,
-  vite: () => ({}),
+  manifest: {
+    name: import.meta.env.APP_NAME,
+    options_page: "options.html",
+    permissions: [],
+  },
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
 });
