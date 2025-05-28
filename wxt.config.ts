a476e1d616edddddd0b6c050b05646138f6265e1
nekn3x <43030839+NEKN3X@users.noreply.github.com>
2025-05-28 13:01:14 +0900

    format

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,14 +1,14 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "wxt";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: "src",
-  modules: ["@wxt-dev/module-react"],
+  srcDir: 'src',
+  modules: ['@wxt-dev/module-react'],
   imports: false,
   manifest: {
     name: import.meta.env.APP_NAME,
-    options_page: "options.html",
+    options_page: 'options.html',
     permissions: [],
   },
   vite: () => ({
