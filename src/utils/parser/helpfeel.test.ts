3ee8af649588b523d30109ff42c1d7c339508440
nekn3x <43030839+NEKN3X@users.noreply.github.com>
2025-05-29 09:43:22 +0900

    fix helpfeel parser

--- src/utils/parser/helpfeel.test.ts
+++ src/utils/parser/helpfeel.test.ts
@@ -1,12 +1,16 @@
 import { expect, test } from 'vitest';
-import { literal, synonym } from './helpfeel';
+import { factor, literal, synonym } from './helpfeel';
 
 test('factor', () => {
-  expect(synonym('abc')).toEqual([[['abc'], '']]);
-  expect(synonym('abc(xyz|123)')).toEqual([[['abc'], '(xyz|123)']]);
-  expect(synonym('abc(xyz|123)def')).toEqual([[['abc'], '(xyz|123)def']]);
-  expect(synonym('(|xyz)')).toEqual([[['', 'xyz'], '']]);
+  expect(factor('abc')).toEqual([[['abc'], '']]);
+  expect(factor('abc(xyz|123)')).toEqual([[['abc'], '(xyz|123)']]);
+  expect(factor('abc(xyz|123)def')).toEqual([[['abc'], '(xyz|123)def']]);
+  expect(factor('(|xyz)')).toEqual([[['', 'xyz'], '']]);
+  expect(factor('(abc(xyz|123))あいう')).toEqual([
+    [['abcxyz', 'abc123'], 'あいう'],
+  ]);
 });
+
 test('synonym', () => {
   expect(synonym('abc')).toEqual([[['abc'], '']]);
   expect(synonym('abc(xyz|123)')).toEqual([[['abc'], '(xyz|123)']]);
@@ -15,7 +19,11 @@ test('synonym', () => {
   expect(synonym('(xyz|123|あいう)abc')).toEqual([
     [['xyz', '123', 'あいう'], 'abc'],
   ]);
+  expect(synonym('(abc(xyz|123))あいう')).toEqual([
+    [['abcxyz', 'abc123'], 'あいう'],
+  ]);
 });
+
 test('literal', () => {
   expect(literal('abc')).toEqual([[['abc'], '']]);
   expect(literal('(xyz)')).toEqual([[['xyz'], '']]);
@@ -28,5 +36,11 @@ test('literal', () => {
   ]);
   expect(literal('abc(xyz|123)def')).toEqual([
     [['abcxyzdef', 'abc123def'], ''],
+  ]);
+  expect(literal('(abc(xyz|123))あいう')).toEqual([
+    [['abcxyzあいう', 'abc123あいう'], ''],
+  ]);
+  expect(literal('(abc(|123))あいう')).toEqual([
+    [['abcあいう', 'abc123あいう'], ''],
   ]);
 });
--- src/utils/parser/helpfeel.ts
+++ src/utils/parser/helpfeel.ts
@@ -8,64 +8,36 @@ import {
   symbol,
 } from 'ts-monadic-parser';
 
-// helpfeel記法を展開する
-// "? ログイン方法がわからない"
-// -> ["ログイン方法がわからない"]
-// "? (ログイン|サインイン)方法がわからない"
-// -> ["ログイン方法がわからない", "サインイン方法がわからない"]
-// "? (ログイン|:サインイン)方法がわからない"
-// -> ["ログイン方法がわからない", "サインイン方法がわからない"]
-// "? [質問]と記載する"
-// -> ["質問と記載する"]
-// Glossary
-// pc: ([パソコン]|[PC])
-// destroy: ([廃棄]する|捨てる)
-// "? {pc}を{destroy}にはどうすればいいですか？"
-// -> [
-// "パソコンを廃棄するにはどうすればいいですか？",
-// "パソコンを捨てるにはどうすればいいですか？",
-// "PCを廃棄するにはどうすればいいですか？",
-// "PCを捨てるにはどうすればいいですか？",
-// ]
-// 記法
-// "? ": 最初は"? "で始まる
-// (word1|word2): 言い換え表現
-// {word}: Glossary
-// 無視する記法
-// [word]: 検索の強化
-// (word1|:word2): 劣後
-
-// 1. [[], 'xyz(abc|123|あいう)']
-// 2. [['xyzabc'], 'xyz(123|あいう)']
-// 3. [['xyzabc', 'xyz123'], 'xyz(あいう)']
-// 4. [['xyzabc', 'xyz123', 'xyzあいう'], '']
-
 const letter = sat((x) => /[^()|]/.test(x));
 const str: Parser<string[]> = bind(some(letter), (s) => pure([s.join('')]));
 
+// factor = '('literal')' | str
 export const factor: Parser<string[]> = orElse(
   bind(symbol('('), () =>
-    bind(synonym, (syn) => bind(symbol(')'), () => pure(syn))),
+    bind(literal, (lit) => bind(symbol(')'), () => pure(lit))),
   ),
   str,
 );
 
+// synonym = factor(+synonym | ε)
 export const synonym: Parser<string[]> = bind(
   orElse(
     factor,
     bind(symbol('|'), () => bind(synonym, (syn) => pure([''].concat(syn)))),
   ),
-  (f) => {
-    return orElse(
+  (f) =>
+    orElse(
       bind(symbol('|'), () => bind(synonym, (syn) => pure(f.concat(syn)))),
       pure(f),
-    );
-  },
+    ),
 );
 
-export const literal: Parser<string[]> = bind(factor, (f) => {
+// literal = synonym(*literal | ε)
+export const literal: Parser<string[]> = bind(synonym, (syn) => {
   return orElse(
-    bind(literal, (lit) => pure(f.flatMap((x) => lit.map((y) => x.concat(y))))),
-    pure(f),
+    bind(literal, (lit) =>
+      pure(syn.flatMap((x) => lit.map((y) => x.concat(y)))),
+    ),
+    pure(syn),
   );
 });
